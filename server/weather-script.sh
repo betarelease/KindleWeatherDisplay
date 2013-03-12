ede112284debc08654334fe8e6c3b01c2535fe70
Sudhindra Rao [betarelease] <sudhindra.r.rao@gmail.com>
2013-03-12 11:40:07 -0700

    ruby script to process xml and svg - simpler easier than python, lat/long are now params

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -2,7 +2,9 @@
 
 cd "$(dirname "$0")"
 
-python2 weather-script.py 37.5483 -121.9875
-convert -background white weather-script-output.svg weather-script-output.png 
+python weather-script.py 37.5483 -121.9875
+#convert -background white weather-script-output.svg weather-script-output.png 
+rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
 pngcrush -c0 w4 weather-script-output.png weather-output.png
-cp -f weather-output.png /var/www/weather-script-output.png
+cp -f weather-output.png ~/Sites/weather-script-output.png
+#cp -f weather-output.png /var/www/weather-script-output.png
