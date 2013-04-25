3cfcdf53f2b11afa153defc9189b62b91d370c09
Sheroy <smarker@thoughtworks.com>
2013-04-24 23:24:42 -0700

    simple thread to run the weather gathering at regular intervals

--- server/server.rb
+++ server/server.rb
@@ -3,13 +3,15 @@ require './weather'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 
-get '/' do
-  weather = Weather.new(37.5483, -121.9875)
-  weather.fetch
-  weather.process_svg
-  `rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg`
-  `pngcrush -c0 w4 weather-script-output.png weather-output.png`
-  `cp -f weather-output.png ~/Sites/weather-script-output.png`
-  `cp -f weather-output.png #{File.join(File.dirname(__FILE__) + "/public/weather-script-output.png")}`
-  "Latest weather updated"
+Thread.new do
+  while true do
+     sleep 15
+     weather = Weather.new(37.5483, -121.9875)
+     weather.fetch
+     weather.process_svg
+     `rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg`
+     `pngcrush -c0 w4 weather-script-output.png weather-output.png`
+     `cp -f weather-output.png ~/Sites/weather-script-output.png`
+     `cp -f weather-output.png #{File.join(File.dirname(__FILE__) + "/public/weather-script-output.png")}`
+  end
 end
