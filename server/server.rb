22b58cf20d50feb7b1a1851973ae7ce7ff63ed9d
Sheroy <smarker@thoughtworks.com>
2013-04-24 23:14:13 -0700

    fixed ruby server to also publish on sinatra's port

--- server/server.rb
+++ server/server.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
-require 'weather'
+require './weather'
+
+set :public_folder, File.dirname(__FILE__) + '/public'
 
 get '/' do
   weather = Weather.new(37.5483, -121.9875)
@@ -8,4 +10,6 @@ get '/' do
   `rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg`
   `pngcrush -c0 w4 weather-script-output.png weather-output.png`
   `cp -f weather-output.png ~/Sites/weather-script-output.png`
+  `cp -f weather-output.png #{File.join(File.dirname(__FILE__) + "/public/weather-script-output.png")}`
+  "Latest weather updated"
 end
